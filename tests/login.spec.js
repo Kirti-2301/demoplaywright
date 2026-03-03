d519784605fc1cc0200a1bf749d599cf23d89b3b
kirti.verma <kirti.verma@symtrain.com>
2026-03-03 09:48:31 -0500

    code fix

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,7 +5,7 @@ test("Login test", async ({ page }) => {
 
   const loginPage = new LoginPage(page);
 
-  await loginPage.navigate();
+  await loginPage.navigate("https://symtrain.io/");
   await loginPage.login("kirtisuperorgadmin", "Kirti@2301");
   await loginPage.verifyLogin();
 
